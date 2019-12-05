7f3121b5e2866c7403ad23e46ce37f96060b794d
Doyeong <jeekc0308@gmail.com>
2019-12-05 13:46:20 +0900

    JS-Cookie Removed.

--- src/store/reducer/Language.ts
+++ src/store/reducer/Language.ts
@@ -1,9 +1,6 @@
-import Cookies from "js-cookie"
-
 const CHANGE_LANGUAGE = "Language/CHANGE_LANGUAGE" as const
 
 export function dispatchChangeLanguage(lang: "ko" | "en") {
-  Cookies.set("lang", lang)
   return {
     type: CHANGE_LANGUAGE,
     lang,
@@ -16,7 +13,7 @@ export interface LanguageType {
 }
 
 const initialState: LanguageType = {
-  lang: (Cookies.get("lang") as "ko" | "en") || "ko",
+  lang: "ko",
 }
 
 export default function(state = initialState, action: ActionType) {
