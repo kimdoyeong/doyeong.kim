928568c158d13504d8949069c39a75049d908039
jeekc0308 <jeekc0308@gmail.com>
2020-04-24 19:33:55 +0900

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,12 @@ module.exports = {
     },
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-164497611-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
